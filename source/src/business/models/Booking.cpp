bd98fcc4590075416a0b1a0b3d5d1227c4ed229d
Toan Huynh <toanhuynh.apr@gmail.com>
2025-04-21 22:31:15 +0700

    Implement Booking class with getters and setters for travel bookings

---
+++ source/src/business/models/Booking.cpp
@@ -0,0 +1,73 @@
+#include "Booking.h"
+
+Booking::Booking(int id, int userId, int tripId, const std::string &bookingDate,
+                 int numberOfPeople, double totalCost, const std::string &status)
+    : _id(id), _userId(userId), _tripId(tripId), _bookingDate(bookingDate),
+      _numberOfPeople(numberOfPeople), _totalCost(totalCost), _status(status)
+{
+}
+
+int Booking::getId() const
+{
+    return _id;
+}
+
+int Booking::getUserId() const
+{
+    return _userId;
+}
+
+int Booking::getTripId() const
+{
+    return _tripId;
+}
+
+std::string Booking::getBookingDate() const
+{
+    return _bookingDate;
+}
+
+int Booking::getNumberOfPeople() const
+{
+    return _numberOfPeople;
+}
+
+double Booking::getTotalCost() const
+{
+    return _totalCost;
+}
+
+std::string Booking::getStatus() const
+{
+    return _status;
+}
+
+void Booking::setUserId(int userId)
+{
+    _userId = userId;
+}
+
+void Booking::setTripId(int tripId)
+{
+    _tripId = tripId;
+}
+
+void Booking::setBookingDate(const std::string &bookingDate)
+{
+    _bookingDate = bookingDate;
+}
+
+void Booking::setNumberOfPeople(int numberOfPeople)
+{
+    _numberOfPeople = numberOfPeople;
+}
+
+void Booking::setTotalCost(double totalCost)
+{
+    _totalCost = totalCost;
+}
+
+void Booking::setStatus(const std::string &status)
+{
+    _status = status;
+}
