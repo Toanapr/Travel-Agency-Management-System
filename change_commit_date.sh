b28e27d5b1ae4e406a0a6943af45df6c1f8578e1
Toan Huynh <toanhuynh.apr@gmail.com>
2025-05-04 22:20:30 +0700

    up

---
+++ change_commit_date.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# filepath: f:/TravelAgency/change_commit_date.sh
+
+NEW_DATE="2025-04-21T23:40:24"
+
+# Amend the most recent commit with the new date
+export GIT_COMMITTER_DATE="$NEW_DATE"
+export GIT_AUTHOR_DATE="$NEW_DATE"
+git commit --amend --no-edit --date="$NEW_DATE"
+
+echo "Commit date has been changed to: $NEW_DATE"
